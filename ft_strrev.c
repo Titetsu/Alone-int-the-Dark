4c72067c41539b9118eebc1476e346c5e3c2623b
wkoelpin <titetsu@yandex.ru>
2019-08-01 16:14:59 +0300

    Перевернуть строку наоборот

---
+++ ft_strrev.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strrev(char *str)
+{
+	int	i;
+	int	len;
+	char	tmp;
+
+	i = 0;
+	len = ft_strlen(str) - 1;
+	while (len > i)
+	{
+		tmp = str[i];
+		str[i] = str[len];
+		str[len] = tmp;
+		len--;
+		i++;
+	}
+	return (str);
+}
+
+int		main()
+{
+	char	ss[100] = "rumiT";
+	char	*s = ss;
+	
+	printf("%s\n", ft_strrev(ss));
+	return (0);
+}
